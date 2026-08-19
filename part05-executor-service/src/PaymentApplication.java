3c25649ac5df859b6b12fca2547f4936cf17065c
JaleelaF <kodesterra@gmail.com>
2026-08-19 20:04:49 +0530

    Part 5- Code updates

--- part05-executor-service/src/PaymentApplication.java
+++ part05-executor-service/src/PaymentApplication.java
@@ -2,7 +2,7 @@ public class PaymentApplication {
     public static void main(String[] args) {
         PaymentExecutor paymentExecutor = new PaymentExecutor();
         for (int i = 1; i <= 25; i++) {
-            PaymentRecord payment = new PaymentRecord("PAY-" + (1000 + i),
+            PaymentRequest payment = new PaymentRequest("PAY-" + (1000 + i),
                     100 + i * 10);
             paymentExecutor.submit(payment);
         }
